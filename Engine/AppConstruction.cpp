4e3dce55f2cba871fabe8821149ec33d0a94a068
JohnnyAsh11 <114207096+JohnnyAsh11@users.noreply.github.com>
2024-12-04 15:12:16 -0500

    Implemented the Constructor, Copy Constructor and Copy Operator for the Application class.

--- Engine/AppConstruction.cpp
+++ Engine/AppConstruction.cpp
@@ -1,13 +1,23 @@
 #include "Application.h"
 
-Application::Application()
-{
-	std::cout << "Constructing" << std::endl;
-}
+Application::Application() { }
 
 Application::Application(Application const& a_aInput)
 {
-	std::cout << "Copy Constructing" << std::endl;
+	// Copying stack allocated data.
+	m_v3Mouse = a_aInput.m_v3Mouse;
+	m_bIsRunning = a_aInput.m_bIsRunning;
+	m_v4ClearColor = a_aInput.m_v4ClearColor;
+
+	// Deep copying the objects stored on the heap.
+	m_pWindow = new sf::Window(
+		sf::VideoMode(
+			a_aInput.m_pWindow->getSize().x, 
+			a_aInput.m_pWindow->getSize().y),
+		"COPY WINDOW",
+		sf::Style::Default,
+		sf::ContextSettings(24));
+	m_sProgramShader = new Shader(*a_aInput.m_sProgramShader);
 }
 
 Application::~Application()
@@ -17,5 +27,21 @@ Application::~Application()
 
 Application& Application::operator=(Application const& a_aInput)
 {
-	return *new Application();
+	// Copying stack allocated data.
+	m_v3Mouse = a_aInput.m_v3Mouse;
+	m_bIsRunning = a_aInput.m_bIsRunning;
+	m_v4ClearColor = a_aInput.m_v4ClearColor;
+
+	// Deep copying the objects stored on the heap.
+	m_pWindow = new sf::Window(
+		sf::VideoMode(
+			a_aInput.m_pWindow->getSize().x,
+			a_aInput.m_pWindow->getSize().y),
+		"COPY WINDOW",
+		sf::Style::Default,
+		sf::ContextSettings(24));
+	m_sProgramShader = new Shader(*a_aInput.m_sProgramShader);
+
+	// Returning this instance of the application.
+	return *this;
 }
