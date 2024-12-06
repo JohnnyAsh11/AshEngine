86bb99599deaaab5708c4fe25fec010d2ab8b191
johnny (pc) <johnnyfagerlin@gmail.com>
2024-12-06 00:17:16 -0500

    Began the Mesh class.

---
+++ ASH/Mesh.h
@@ -0,0 +1,21 @@
+#ifndef __MESH_H_
+#define __MESH_H_
+
+#include "Definitions.h"
+
+namespace ASH
+{
+	/// <summary>
+	/// Contains the most basic data and information for models and primitives.
+	/// Vertex cloud data container.
+	/// </summary>
+	class ASH_API Mesh
+	{
+	private:
+
+	public:
+
+	};
+}
+
+#endif //__MESH_H_
